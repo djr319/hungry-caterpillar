8b7f286ec47eb42d44ea443e8294c4e1326be587
David Ryan (PC) <mail@djryan.eu>
2023-01-08 01:20:25 +0100

    draw emoticon bug

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const canvas = document.getElementById('game-canvas');
 const ctx = canvas.getContext('2d', {alpha: false});
 window.addEventListener("resize", resize);
@@ -7,4 +6,20 @@ resize();
 function resize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+}
+
+let caterpillarX = Math.floor(window.innerWidth/2);
+let caterpillarY = Math.floor(window.innerHeight/2);
+let caterpillarDirection = 0;
+
+drawCaterpillar();
+
+function drawCaterpillar() {
+  // The size of the emoji is set with the font
+  ctx.font = '100px serif'
+  // use these alignment properties for "better" positioning
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  // draw the emoji
+  ctx.fillText('🐛', caterpillarX, caterpillarY);
 }
